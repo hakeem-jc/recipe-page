1190675bdf74c6739d65ccf6dcb3a5797bb7d061
hakeem-jc <hakeem_clarke14@yahoo.com>
2024-01-31 08:33:19 -0500

    Style instructions

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
             </ul>
           </article>
 
-          <article className={styles.recipeSection}>
+          <article className={styles.ingredientSection}>
             <h2>Ingredients</h2>
             <ul className={styles.listItem}>
               <li>2-3 large eggs</li>
@@ -41,9 +41,9 @@ export default function Home() {
             </ul>
           </article>
 
-          <article className={styles.recipeSection}>
+          <article className={styles.instructionSection}>
             <h2>Instructions</h2>
-            <ol>
+            <ol className={styles.listItem} >
               <li>
                 <span className={styles.bold}>Beat the eggs</span>: In a bowl, beat the eggs with a pinch of salt and pepper until they are well mixed. You can add a tablespoon of water or milk for a fluffier texture.
               </li>
@@ -54,18 +54,18 @@ export default function Home() {
                 <span className={styles.bold}>Cook the omelette:</span> Once the butter is melted and bubbling, pour in the eggs. Tilt the pan to ensure the eggs evenly coat the surface.
               </li>
               <li>
-                <span className={styles.bold}>Add fillings (optional):</span>When the eggs begin to set at the edges but are still slightly runny in the middle, sprinkle your chosen fillings over one half of the omelette.
+                <span className={styles.bold}>Add fillings (optional):</span> When the eggs begin to set at the edges but are still slightly runny in the middle, sprinkle your chosen fillings over one half of the omelette.
               </li>
               <li>
-                <span className={styles.bold}>Fold and serve:</span>As the omelette continues to cook, carefully lift one edge and fold it over the fillings. Let it cook for another minute, then slide it onto a plate.
+                <span className={styles.bold}>Fold and serve:</span> As the omelette continues to cook, carefully lift one edge and fold it over the fillings. Let it cook for another minute, then slide it onto a plate.
               </li>
               <li>
-                <span className={styles.bold}>Enjoy:</span>Serve hot, with additional salt and pepper if needed.
+                <span className={styles.bold}>Enjoy:</span> Serve hot, with additional salt and pepper if needed.
               </li>
             </ol>
           </article>
 
-          <article className={styles.recipeSection}>
+          <article>
             <h2>Nutrition</h2>
             <p>The table below shows nutritional values per serving without the additional fillings.</p>
             <div>
