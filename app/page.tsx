e52f011c383e6d16fdae61b0cfc7aca0583cd446
hakeem-jc <hakeem_clarke14@yahoo.com>
2024-01-31 08:02:10 -0500

    Style ingredients

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
 
           <article className={styles.prepTime}>
             <h2 className={styles.prepTitle}>Preparation time</h2>
-            <ul className={styles.prepList}>
+            <ul className={styles.listItem}>
               <li><span className={styles.bold}>Total:</span> Approximately 10 minutes</li>
               <li><span className={styles.bold}>Preparation:</span> 5 minutes</li>
               <li><span className={styles.bold}>Cooking:</span> 5 minutes</li>
@@ -32,7 +32,7 @@ export default function Home() {
 
           <article className={styles.recipeSection}>
             <h2>Ingredients</h2>
-            <ul>
+            <ul className={styles.listItem}>
               <li>2-3 large eggs</li>
               <li>Salt, to taste</li>
               <li>Pepper, to taste</li>
@@ -45,22 +45,22 @@ export default function Home() {
             <h2>Instructions</h2>
             <ol>
               <li>
-              <span className={styles.bold}>Beat the eggs</span>: In a bowl, beat the eggs with a pinch of salt and pepper until they are well mixed. You can add a tablespoon of water or milk for a fluffier texture.
+                <span className={styles.bold}>Beat the eggs</span>: In a bowl, beat the eggs with a pinch of salt and pepper until they are well mixed. You can add a tablespoon of water or milk for a fluffier texture.
               </li>
               <li>
-              <span className={styles.bold}>Heat the pan:</span> Place a non-stick frying pan over medium heat and add butter or oil.
+                <span className={styles.bold}>Heat the pan:</span> Place a non-stick frying pan over medium heat and add butter or oil.
               </li>
               <li>
-              <span className={styles.bold}>Cook the omelette:</span> Once the butter is melted and bubbling, pour in the eggs. Tilt the pan to ensure the eggs evenly coat the surface.
+                <span className={styles.bold}>Cook the omelette:</span> Once the butter is melted and bubbling, pour in the eggs. Tilt the pan to ensure the eggs evenly coat the surface.
               </li>
               <li>
-              <span className={styles.bold}>Add fillings (optional):</span>When the eggs begin to set at the edges but are still slightly runny in the middle, sprinkle your chosen fillings over one half of the omelette.
+                <span className={styles.bold}>Add fillings (optional):</span>When the eggs begin to set at the edges but are still slightly runny in the middle, sprinkle your chosen fillings over one half of the omelette.
               </li>
               <li>
-              <span className={styles.bold}>Fold and serve:</span>As the omelette continues to cook, carefully lift one edge and fold it over the fillings. Let it cook for another minute, then slide it onto a plate.
+                <span className={styles.bold}>Fold and serve:</span>As the omelette continues to cook, carefully lift one edge and fold it over the fillings. Let it cook for another minute, then slide it onto a plate.
               </li>
               <li>
-              <span className={styles.bold}>Enjoy:</span>Serve hot, with additional salt and pepper if needed.
+                <span className={styles.bold}>Enjoy:</span>Serve hot, with additional salt and pepper if needed.
               </li>
             </ol>
           </article>
